0796e437564d66a864d27a504e8c5cf3ac1fa8d2
Eric Xie <exie102202@gmail.com>
2025-05-31 00:53:32 -0400

    Initial template

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import plans
 
 app = FastAPI()
 app.include_router(plans.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # React dev server origin
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
